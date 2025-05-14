a255dff25c72147cfcb175930f3433dc3bed4af0
Juan Carlos <juan.carlos.dev@outlook.com>
2025-05-14 17:01:25 -0300

    feat: front and back end edited

--- Projeto_Rota_Kids/TelaCadastro/TelaCadastro/ContentView.swift
+++ Projeto_Rota_Kids/TelaCadastro/TelaCadastro/ContentView.swift
@@ -8,95 +8,150 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State public var nome: String = ""
+    @State public var telefone: String = ""
+    @State public var endereco: String = ""
+    @State public var apelido: String = ""
+    @State public var observacoes: String = ""
+
+    
+    
+    
     var body: some View {
         
         ZStack{
             Color.amarelo
                 .ignoresSafeArea()
-        
-            VStack (alignment: .leading){
+            
+            VStack{
                 
-                VStack{
+                VStack(alignment: .leading){
                     HStack{
                         Image("RotaKids")
                             .resizable()
                             .scaledToFit()
                             .scaledToFill()
-                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 100)
+                            .frame(width: 80,height: 80)
                         
-                    }
-                    Divider()
-                        .bold()
-                        .foregroundColor(.black)
+                        Spacer()
+                        
+                    }.padding(.horizontal)
+                        .background()
                 }
-                
-                
-                VStack(alignment: .leading){
-                   
-                    
-                    HStack{
-                        Text("Nome do Responsável:")
-                            .bold()
-                            .font(.system(size: 22))
+         
+                    ScrollView{
+          
+                        
+                        VStack(alignment: .leading){
                             
-                    }
-                    HStack{
                             
-                        Text("text field")
+                            
+                            Text("Nome do Responsável:")
+                                .foregroundColor(.letra)
+                                .bold()
+                                .font(.system(size: 22))
+                            
+                            
+                            TextField("  Nome:", text: $nome)
+                                .background(Rectangle()
+                                    .foregroundColor(.white)
+                                    .frame(height: 30)
+                                    .cornerRadius(50))
+                            
+                            
+                            Text("Telefone do Responsável:")
+                                .bold()
+                                .foregroundColor(.letra)
+                                .font(.system(size: 22))
+                            
+                            TextField("  Telefone:", text: $telefone)
+                                .background(Rectangle()
+                                    .foregroundColor(.white)
+                                    .frame(height: 30)
+                                    .cornerRadius(50))
+                            
+                            
+                            
+                            Text("Endereço:")
+                                .bold()
+                                .foregroundColor(.letra)
+                                .font(.system(size: 22))
+                            
+                            TextField("  Endereço:", text: $endereco)
+                                .background(Rectangle()
+                                    .foregroundColor(.white)
+                                    .frame(height: 30)
+                                    .cornerRadius(50))
+                            
+                            
+                            
+                            Text("Criança:")
+                                .bold()
+                                .foregroundColor(.letra)
+                                .font(.system(size: 22))
+                            
+                            TextField("  Nome:", text: $apelido)
+                                .background(Rectangle()
+                                    .foregroundColor(.white)
+                                    .frame(height: 30)
+                                    .cornerRadius(50))
+                            
+                            
+                            
+                            Text("Foto da criança:")
+                                .bold()
+                                .foregroundColor(.letra)
+                                .font(.system(size: 22))
+                            
+                            
+                            
+                            Text("Observações:")
+                                .bold()
+                                .foregroundColor(.letra)
+                                .font(.system(size: 22))
+                            
+                            TextField("  Observacoes:", text: $observacoes, axis: .vertical)
+                                .textFieldStyle(.roundedBorder)
+                            
+                            Spacer(minLength: 40)
+                            
+                            
+                            
+                            
 
+                        }.padding(.horizontal)
+                        
+                        HStack{
+                            
+                            Spacer()
+                            
+                            Button("Cadastrar"){}.foregroundColor(.white)
+                                .bold()
+                                .background(Rectangle()
+                                    .foregroundColor(.botao)
+                                    .cornerRadius(50)
+                                    .frame(width: 150, height:50))
+                                    .padding(.trailing, 40)
+                            
+                            
+                            
+                        }.padding(.horizontal)
+                        
+                        
+                        Spacer()
+                        
+                            
+                            
                         
                     }
-                    HStack{
-                        Text("Telefone do Responsável:")
-                            .bold()
-                            .font(.system(size: 22))
-
-
-                    }
-                    HStack{
-                        Text("Endereço:")
-                            .bold()
-                            .font(.system(size: 22))
-
-
-                    }
-                    HStack{
-                        Text("Criança:")
-                            .bold()
-                            .font(.system(size: 22))
-
-
-                    }
-                    HStack{
-                        Text("Foto da criança:")
-                            .bold()
-                            .font(.system(size: 22))
-
-
-                    }
-                    HStack{
-                        Text("Observações:")
-                            .bold()
-                            .font(.system(size: 22))
-
-
-                    }
                     
-                    Spacer()
-                    
-                    
-                    
-                    
-                }.padding(.vertical)
                 
-                Spacer()
-                
-            }.padding()
+            }
+            
         }
-        
     }
 }
-
 
 #Preview {
     ContentView()
